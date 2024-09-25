3e52a9d09be65164e67e2594fc897256533974f0
vacasan <kotaracvladan@gmail.com>
2024-09-25 14:33:46 +0200

    feat: configure vite for lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    dts({ tsconfigPath: "./tsconfig.app.json"})
+  ],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+    },
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'knapsack',
+      formats: ['es'],
+    },
+  },
 })
