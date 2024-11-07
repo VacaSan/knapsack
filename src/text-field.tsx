c9301dddd3d4ab5fef17d3299dd96c560e09bf34
vacasan <kotaracvladan@gmail.com>
2024-11-07 12:21:15 +0100

    feat: use semantic colors

--- src/text-field.tsx
+++ src/text-field.tsx
@@ -9,7 +9,7 @@ function TextFieldRoot(props: React.ComponentProps<"div">) {
       <div
         {...props}
         className={cn(
-          "group flex cursor-text rounded-md border border-zinc-950/20 bg-white text-zinc-950 hover:border-zinc-950/30 has-[input:disabled]:cursor-not-allowed has-[input:disabled]:border-zinc-950/20 has-[input:focus]:border-blue-500 has-[input[aria-invalid]]:border-red-500 has-[input:disabled]:bg-zinc-950/5 has-[input:disabled]:text-zinc-500 has-[input:disabled]:opacity-50 has-[input:focus]:ring-1 has-[input[aria-invalid]:focus]:ring-1 has-[input:focus]:ring-blue-500 has-[input[aria-invalid]:focus]:ring-red-500",
+          "group flex cursor-text rounded-md border border-gray-950/20 bg-white text-gray-950 hover:border-gray-950/30 has-[input:disabled]:cursor-not-allowed has-[input:disabled]:border-gray-950/20 has-[input:focus]:border-primary-500/60 has-[input[aria-invalid]]:border-error-500/60 has-[input:disabled]:bg-gray-950/5 has-[input:disabled]:text-gray-500 has-[input:disabled]:opacity-50 has-[input:focus]:ring-2 has-[input[aria-invalid]:focus]:ring-2 has-[input:focus]:ring-primary-500/30 has-[input[aria-invalid]:focus]:ring-error-500/30",
           props.className,
         )}
       />
@@ -30,7 +30,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         <input
           ref={ref}
           {...props}
-          className="peer block flex-1 appearance-none border-0 bg-transparent px-0 py-2 text-sm first:pl-3 last:pr-3 focus:outline-none disabled:cursor-not-allowed disabled:text-zinc-500 disabled:placeholder:text-zinc-500"
+          className="peer block flex-1 appearance-none border-0 bg-transparent px-0 py-2 text-sm first:pl-3 last:pr-3 focus:outline-none disabled:cursor-not-allowed disabled:text-gray-500 disabled:placeholder:text-gray-500"
         />
       </Wrapper>
     );
