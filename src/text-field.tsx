c4c2d4b5be564a9f0728a62f4f7d45224089b704
vacasan <kotaracvladan@gmail.com>
2024-12-08 22:16:50 +0100

    refactor: text-field

--- src/text-field.tsx
+++ src/text-field.tsx
@@ -9,7 +9,7 @@ function TextFieldRoot(props: React.ComponentProps<"div">) {
       <div
         {...props}
         className={cn(
-          "group flex cursor-text rounded-md border border-gray-950/20 bg-white text-gray-950 hover:border-gray-950/30 has-[input:disabled]:cursor-not-allowed has-[input:disabled]:border-gray-950/20 has-[input:focus]:border-primary-500/60 has-[input[aria-invalid]]:border-error-500/60 has-[input:disabled]:bg-gray-950/5 has-[input:disabled]:text-gray-500 has-[input:disabled]:opacity-50 has-[input:focus]:ring-2 has-[input[aria-invalid]:focus]:ring-2 has-[input:focus]:ring-primary-500/30 has-[input[aria-invalid]:focus]:ring-error-500/30",
+          "group flex cursor-text rounded-md border border-graya-7 bg-white text-gray-12 hover:border-graya-8 has-[input:disabled]:cursor-not-allowed has-[input:disabled]:border-graya-6 has-[input:focus]:border-primarya-9 has-[input[aria-invalid]]:border-errora-8 has-[input:disabled]:bg-graya-2 has-[input:disabled]:text-gray-11 has-[input:disabled]:opacity-50 has-[input:focus]:ring-2 has-[input[aria-invalid]:focus]:ring-2 has-[input:focus]:ring-primarya-6 has-[input[aria-invalid]:focus]:ring-errora-6",
           props.className,
         )}
       />
@@ -30,7 +30,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         <input
           ref={ref}
           {...props}
-          className="peer block flex-1 appearance-none border-0 bg-transparent px-0 py-2 text-sm first:pl-3 last:pr-3 focus:outline-none disabled:cursor-not-allowed disabled:text-gray-500 disabled:placeholder:text-gray-500"
+          className="peer block flex-1 appearance-none border-0 bg-transparent px-0 py-2 text-sm first:pl-3 last:pr-3 focus:outline-none disabled:cursor-not-allowed disabled:text-gray-11 disabled:placeholder:text-graya-11"
         />
       </Wrapper>
     );
